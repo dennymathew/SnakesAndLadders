ea8062ade733aa2c35ce9ce2286b4ac9876ec2de
Denny Mathew <devdenmathew@gmail.com>
2019-04-08 23:06:02 +0530

    + Setup completed + Added assets + Defined game scene variables

--- SnakeAndLadder/GameScene.swift
+++ SnakeAndLadder/GameScene.swift
@@ -11,100 +11,75 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    var entities = [GKEntity]()
-    var graphs = [String : GKGraph]()
+    //Gameboard
+    var board = SKSpriteNode()
     
-    private var lastUpdateTime : TimeInterval = 0
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    //Player Nodes
+    var player = SKSpriteNode()
+    var computer = SKSpriteNode()
     
-    override func sceneDidLoad() {
+    //Game State Variables
+    var isPlayerTurn: Bool = true
+    var isRolling: Bool = false
+    var moveFinished: Bool = true
+    var isGameOver: Bool = false
+    
+    //Player Positions
+    var playerPosition: Int = 0
+    var computerPosition: Int = 0
+    
+    //Player Labels
+    var playerLabel = SKLabelNode()
+    var computerLabel = SKLabelNode()
+    var playerPositionLabel = SKLabelNode()
+    var computerPositionLabel = SKLabelNode()
+    
+    //Game Sounds
+    var diceSound = SKAction()
+    var snakeSound = SKAction()
+    var ladderSound = SKAction()
+    var moveSound = SKAction()
+    var introSound = SKAction()
+    var lostSound = SKAction()
+    var gameFlowSound = SKAction()
+    
+    //New Postions
+    let snakePositions = [25: 3, 56: 48, 59: 1, 69:32, 83: 57, 91: 73, 94: 26, 99: 80]
+    let ladderPositions = [7: 11, 20: 38, 28:65, 36: 44, 42: 63, 51: 67, 62: 81, 71: 90, 86: 97]
+    
+    //Z Positions
+    enum ObjectZPosition: CGFloat {
+        case board = 0
+        case players = 1
+        case labels = 2
+        case dice = 3
+    }
+    
+    override func didMove(to view: SKView) {
+        self.backgroundColor = .black
+        setupBoard()
+        setupSounds()
+    }
+}
 
-        self.lastUpdateTime = 0
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+//MARK: - Setups
+extension GameScene {
+    
+    private func setupBoard() {
+        board = SKSpriteNode(imageNamed: "Gameboard")
+        let height = self.frame.width / 2.2
+        board.size = CGSize(width: height, height: height)
+        board.zPosition = ObjectZPosition.board.rawValue
+        addChild(board)
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-        
-        // Initialize _lastUpdateTime if it has not already been
-        if (self.lastUpdateTime == 0) {
-            self.lastUpdateTime = currentTime
-        }
-        
-        // Calculate time since last update
-        let dt = currentTime - self.lastUpdateTime
-        
-        // Update entities
-        for entity in self.entities {
-            entity.update(deltaTime: dt)
-        }
-        
-        self.lastUpdateTime = currentTime
+    private func setupSounds() {
+        diceSound = SKAction.playSoundFileNamed("Dice.mp3", waitForCompletion: false)
+        snakeSound = SKAction.playSoundFileNamed("Snake.wav", waitForCompletion: false)
+        ladderSound = SKAction.playSoundFileNamed("Ladder", waitForCompletion: false)
+        moveSound = SKAction.playSoundFileNamed("Move.wav", waitForCompletion: false)
+        lostSound = SKAction.playSoundFileNamed("Lost.wav", waitForCompletion: false)
+        introSound = SKAction.playSoundFileNamed("Intro.wav", waitForCompletion: false)
+        gameFlowSound = SKAction.playSoundFileNamed("Gameflow.wav", waitForCompletion: false)
     }
 }
